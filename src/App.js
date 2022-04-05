404a492fecd308e77b53224142d890ccebed760c
LuvumboChristopher <christopherluvumbo.dev@gmail.com>
2022-04-05 17:38:20 +0200

    1 file modified, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React , {useState, useEffect} from "react";
 import axios from 'axios'
 import "./style.css";
-import Quote from './components/Quote'
 
 export default function App() {
 
   const [isLoading, setisLoading]= useState(true)
-  const [quote, setQuote] = useState([])
+  const [person, setPerson] = useState({})
   const [clicked, setClicked]= useState(false)
 
   const handleClick = ()=> {
     setClicked(!clicked)
   }
 
-  const getQuote = ()=> {
-    axios
+  const getQuote = async()=> {
+    await axios
       .get('https://simpsons-quotes-api.herokuapp.com/quotes?count=1')
       .then((response) => response.data)
       .then((data) => {
-        setQuote(data[0].quote);
+        console.log(data[0])
+        setPerson(data[0]);
         setisLoading(false)
       });
   }
@@ -32,8 +32,12 @@ export default function App() {
   }, [clicked])  
 
   return (
+    
     <>
-      {isLoading ? <p>Loading...</p>: <Quote quote={quote}/>}
+      {isLoading ? <p>Loading...</p> : <div>
+      <img src={person.image} alt={person.character}/>
+      <h1>{person.character}</h1>
+      <p>{person.quote}</p> </div>}
       {!isLoading && <button onClick={handleClick}>Another one</button>}
     </>
   );
